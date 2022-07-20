c8da02c94dad3341e50d68a3a35cf951a03fb14a
Yangyu Chen <cyy@cyyself.name>
2022-07-20 16:07:31 +0800

    add running mode support

--- src/sim_mycpu.cpp
+++ src/sim_mycpu.cpp
@@ -12,6 +12,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <thread>
+#include <csignal>
 
 void connect_wire(axi4_ptr <32,32,4> &mmio_ptr, Vmycpu_top *top) {
     // connect
@@ -52,50 +53,27 @@ void connect_wire(axi4_ptr <32,32,4> &mmio_ptr, Vmycpu_top *top) {
     mmio_ptr.rvalid     = &(top->rvalid);
 }
 
+bool running = true;
+bool trace_on = false;
+bool trace_pc = false;
+long sim_time = 1e3;
+
 void uart_input(uart8250 &uart) {
     termios tmp;
     tcgetattr(STDIN_FILENO,&tmp);
     tmp.c_lflag &=(~ICANON & ~ECHO);
     tcsetattr(STDIN_FILENO,TCSANOW,&tmp);
-    while (1) {
+    while (running) {
         char c = getchar();
         if (c == 10) c = 13; // convert lf to cr
         uart.putc(c);
     }
 }
 
-bool running = true;
-
-int main(int argc, char** argv, char** env) {
-    Verilated::commandArgs(argc, argv);
-    // setup trace
-    bool trace_on = false;
-    bool trace_pc = false;
-    long sim_time = 1e3;
-    for (int i=1;i<argc;i++) {
-        if (strcmp(argv[i],"-trace") == 0) {
-            trace_on = true;
-            if (i+1 < argc) {
-                sscanf(argv[i+1],"%lu",&sim_time);
-            }
-        }
-        else if (strcmp(argv[i],"-pc") == 0) {
-            trace_pc = true;
-        }
-    }
-    Verilated::traceEverOn(trace_on);
-    // setup soc
-    Vmycpu_top *top = new Vmycpu_top;
-    axi4_ptr <32,32,4> mmio_ptr;
-
-    connect_wire(mmio_ptr,top);
-    assert(mmio_ptr.check());
-    
-    axi4_ref <32,32,4> mmio_ref(mmio_ptr);
+void system_test(Vmycpu_top *top, axi4_ref <32,32,4> &mmio_ref) {
     axi4     <32,32,4> mmio_sigs;
     axi4_ref <32,32,4> mmio_sigs_ref(mmio_sigs);
     axi4_xbar<32,32,4> mmio;
-
 
     // uart8250 at 0x1fe40000 (APB)
     uart8250 uart;
@@ -153,6 +131,49 @@ int main(int argc, char** argv, char** env) {
     }
     if (trace_on) vcd.close();
     top->final();
-    uart_input_thread->~thread();
+    pthread_kill(uart_input_thread->native_handle(),SIGKILL);
+}
+
+int main(int argc, char** argv, char** env) {
+    Verilated::commandArgs(argc, argv);
+
+    std::signal(SIGINT, [](int) {
+        running = false;
+    });
+
+    enum {FUNC, PERF, SYS_TEST, RUN_OS} run_mode;
+
+    for (int i=1;i<argc;i++) {
+        if (strcmp(argv[i],"-trace") == 0) {
+            trace_on = true;
+            if (i+1 < argc) {
+                sscanf(argv[i++],"%lu",&sim_time);
+            }
+        }
+        else if (strcmp(argv[i],"-pc") == 0) {
+            trace_pc = true;
+        }
+        else if (strcmp(argv[i],"-sys") == 0) {
+            run_mode = SYS_TEST;
+        }
+    }
+    Verilated::traceEverOn(trace_on);
+    // setup soc
+    Vmycpu_top *top = new Vmycpu_top;
+    axi4_ptr <32,32,4> mmio_ptr;
+
+    connect_wire(mmio_ptr,top);
+    assert(mmio_ptr.check());
+    
+    axi4_ref <32,32,4> mmio_ref(mmio_ptr);
+
+    switch (run_mode) {
+        case SYS_TEST:
+            system_test(top, mmio_ref);
+            break;
+        default:
+            printf("Unknown running mode.\n");
+            exit(-ENOENT);
+    }
     return 0;
 }
