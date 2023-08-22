d883b532018392e7f8f69f5b1128f081d3ce1900
Yangyu Chen <cyy@cyyself.name>
2023-08-23 00:45:40 +0800

    warp params in auto_sig with brackets

--- src/auto_sig.hpp
+++ src/auto_sig.hpp
@@ -4,9 +4,9 @@
 #include <condition_variable>
 
 #define AUTO_TYPE(msb, lsb) \
-    typename std::conditional <(msb-lsb+1) <=  8, CData, \
-    typename std::conditional <(msb-lsb+1) <= 16, SData, \
-    typename std::conditional <(msb-lsb+1) <= 32, IData, QData >::type >::type >::type
+    typename std::conditional <((msb)-(lsb)+1) <=  8, CData, \
+    typename std::conditional <((msb)-(lsb)+1) <= 16, SData, \
+    typename std::conditional <((msb)-(lsb)+1) <= 32, IData, QData >::type >::type >::type
 
 #define AUTO_SIG(name, msb, lsb) AUTO_TYPE(msb,lsb) name
 
